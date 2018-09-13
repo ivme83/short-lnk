95b36ad026af3e523dbb06cc7e4c88cf2d102322
Roland Willis <rolandwillisiv@gmail.com>
2018-09-13 16:50:25 -0700

    set up redirects and shortid

--- imports/ui/LinkPage.js
+++ imports/ui/LinkPage.js
@@ -4,6 +4,9 @@ import { Meteor }       from 'meteor/meteor';
 
 import browserHistory   from '../utils/History';
 
+import { Links }        from '../api/links';
+import LinksList        from './LinksList';
+
 
 export default class LinkPage extends React.Component {
     onLogout() {
@@ -16,12 +19,29 @@ export default class LinkPage extends React.Component {
         }
     }
 
+    onSubmit(e) {
+        const url = this.refs.url.value.trim();
+
+        e.preventDefault();
+
+        if (url) {
+            Meteor.call('links.insert', url);
+            this.refs.url.value = '';
+        }
+    }
+
     render() {
         return (
             <div>
                 <h1>Your Links</h1>
 
                 <button onClick={this.onLogout.bind(this)} >Logout</button>
+                <LinksList />
+                <p>Add Link</p>
+                <form onSubmit={this.onSubmit.bind(this)} >
+                    <input type="text" ref="url" placeholder="URL" />
+                    <button>Add Link</button>
+                </form>
             </div>
         );
     }
